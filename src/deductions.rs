8a58b0f2724e8d8b2301125b7d48c117e3f8180c
Lowell Thoerner <lowellashton@gmail.com>
2023-01-18 12:36:08 -0800

    Added annotations for example output.

--- src/deductions.rs
+++ src/deductions.rs
@@ -139,35 +139,35 @@ f = TRUE
 
 =>
 (m ∧ ¬b) → j
-(TRUE ∨ s) → m
+(TRUE ∨ s) → m [SUBSTITUTE]
 b → t
-TRUE → ¬t
+TRUE → ¬t [SUBSTITUTE]
 
 =>
 (m ∧ ¬b) → j
-TRUE → m
+TRUE → m [EVALUATE]
 b → t
-t = FALSE
+t = FALSE [EVALUATE]
 
 
 =>
 (m ∧ ¬b) → j
-m = TRUE
-b → FALSE
+m = TRUE [EVALUATE]
+b → FALSE [SUBSTITUTE]
 
 =>
-(TRUE ∧ ¬b) → j
-b = FALSE
+(TRUE ∧ ¬b) → j [SUBSTITUTE]
+b = FALSE [EVALUATE]
 
 =>
-(TRUE ∧ ¬FALSE) → j
+(TRUE ∧ ¬FALSE) → j [SUBSTITUTE]
 
 =>
-(TRUE ∧ TRUE) → j
+(TRUE ∧ TRUE) → j [EVALUATE]
 
 =>
-TRUE → j
+TRUE → j [EVALUATE]
 
 =>
-j = TRUE
+j = TRUE [EVALUATE]
 */
